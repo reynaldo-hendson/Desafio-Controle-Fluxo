7ab9f547bea14a2122ec5484bb7258287d6973d3
ReynaldoHendson <80369346+reynaldo86@users.noreply.github.com>
2023-09-01 16:10:48 -0300

    bug: fixes input to only receive integers.

--- DesafioControleFluxo/src/domain/Contador.java
+++ DesafioControleFluxo/src/domain/Contador.java
@@ -18,25 +18,36 @@ public class Contador {
         System.out.println("-------------------Desafio Controle de fluxo---------------------");
         System.out.println("Digite dois valores para saber o número de interações entre eles.");
         System.out.println("1º valor: ");
-        int paramentro1 = input.nextInt();
+        int paramentro1 = lerInteiro(input);
 
         System.out.println("2º valor: ");
-        int paramentro2 = input.nextInt();
+        int paramentro2 = lerInteiro(input);
 
         try{
             contar(paramentro1, paramentro2);
-        } catch (ParametrosInvalidosException e) {
+        }catch (ParametrosInvalidosException e){
             System.out.println("Erro: " + e.getMessage());
         }
     }
 
     public static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
-        if (parametroUm >= parametroDois){
-            throw new ParametrosInvalidosException();
-        } else {
-            int diferenca = parametroDois - parametroUm;
-            for (int i = 1; i <= diferenca; ++i){
-                System.out.println("Imprimindo a "+ i +"ª interação: "+ i);
+
+            if (parametroUm >= parametroDois) {
+                throw new ParametrosInvalidosException();
+            }else {
+                int diferenca = parametroDois - parametroUm;
+                for (int i = 1; i <= diferenca; ++i) {
+                    System.out.println("Imprimindo a " + i + "ª interação: " + i);
+                }
+            }
+    }
+
+    public static int lerInteiro(Scanner scanner) {
+        while (true) {
+            try {
+                return Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Entrada inválida. Digite um número inteiro:");
             }
         }
     }
